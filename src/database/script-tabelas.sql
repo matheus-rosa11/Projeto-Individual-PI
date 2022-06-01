2a5f0016a8dc167785c4c84392663a0d43fcef36
matheus-rosa11 <matheus.rosa@sptech.school>
2022-06-01 17:26:22 -0300

    Adding DB Script w All Tables

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -3,33 +3,53 @@
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
 /* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
+CREATE DATABASE isong;
 
-USE acquatec;
+USE isong;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+CREATE TABLE Usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nomeUsuario varchar(45),
+	emailUsuario varchar(45),
+	senhaUsuario varchar(45),
+	pontos int
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-    descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-); 
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DOUBLE,
-	umidade DOUBLE,
-	momento DATETIME,
-	fk_aquario INT
+CREATE TABLE Produto (
+	idProduto INT PRIMARY KEY AUTO_INCREMENT,
+	nomeProduto varchar(45),
+	descricao varchar(200),
+	preco decimal(10,2)
 );
 
+CREATE TABLE Curso (
+	idCurso INT PRIMARY KEY AUTO_INCREMENT,
+	nomeCurso varchar(45),
+	preco decimal(10,2)
+);
+
+CREATE TABLE Venda (
+	idVenda int auto_increment,
+	fkProduto int,
+	foreign key (fkProduto) references Produto(idProduto),
+	fkUsuario int,
+	foreign key (fkUsuario) references Usuario(idUsuario),
+	primary key (idVenda, fkProduto, fkUsuario),
+	dtVenda date,
+	tipoVenda varchar(45),
+	parcelas int,
+	desconto decimal(10,2)
+);
+
+CREATE TABLE Matricula (
+	idMatricula int auto_increment,
+	fkUsuario int,
+	foreign key (fkUsuario) references Usuario(idUsuario),
+	fkCurso int,
+	foreign key (fkCurso) references Curso(idCurso),
+	primary key(idMatricula, fkUsuario, fkCurso),
+	dtMatricula date
+);
 
 
 
